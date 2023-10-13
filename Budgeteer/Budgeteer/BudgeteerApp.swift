dc9c78dabc80a9548d77c411e253156be75492dc
andrewaustin12 <andrew.d.austin2@gmail.com>
2023-10-13 14:37:36 +0700

    initial commit

---
+++ Budgeteer/Budgeteer/BudgeteerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgeteerApp.swift
+//  Budgeteer
+//
+//  Created by andrew austin on 10/13/23.
+//
+
+import SwiftUI
+
+@main
+struct BudgeteerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
